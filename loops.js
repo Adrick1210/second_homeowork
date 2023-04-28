6895d3b2466f11a8729290003d2d8baff767cd53
Christian Alexander <christianalexander@christians-mbp.lan>
2023-04-27 22:54:10 -0700

    finished loop practice

---
+++ loops.js
@@ -0,0 +1,44 @@
+// ascending
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
+// }
+
+// descending
+// for (let i = 99; i > -1; i--) {
+//     console.log(i);
+// }
+
+// descending evens
+// for (let i = 98; i > -2; i -= 2) {
+//     console.log(i);
+// }
+
+// ascending evens
+// for (let i = 0; i < 99; i+= 2) {
+//     console.log(i);
+// }
+
+// ascending odds
+// for (let i = 0; i < 100; i += 3) {
+//     console.log(i);
+// }
+
+// descending odds
+// for (let i = 100; i > 0; i -= 3) {
+//     console.log(i);
+// }
+
+// ascending 
+// for (let i = 49; i < 72; i++) {
+//     console.log(i);
+// }
+
+// descending
+// for (let i = 81; i > 25; i--) {
+//   console.log(i);
+// }
+
+// ascending *odds
+// for (let i = 3; i < 90; i *= 3) {
+//   console.log(i);
+// }
